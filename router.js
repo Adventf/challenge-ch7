7e3e535bf5d05c8faab39db13ff70a0588033de7
Adventf <adventfajarm@gmail.com>
2022-06-01 19:39:35 +0700

    finishing app

--- router.js
+++ router.js
@@ -2,20 +2,16 @@ const router = require("express").Router();
 const auth = require("./controllers/authControllers");
 const restrict = require("./middlewares/restrict");
 
-router.get("/", restrict, (req, res) => res.render("home"));
+router.get("/", (req, res) => res.render("home", { title: "home" }));
 router.get("/api/v1/auth/whoami", restrict, auth.whoami);
-router.get("/play", (req, res) => res.render("play"));
+router.get("/play", restrict, (req, res) => res.render("play", { title: "play" }));
 router.get("/about", (req, res) => res.render("about", { title: "about" }));
 router.get("/help", (req, res) => res.render("help", { title: "help" }));
 
-router.get("/register", (req, res) =>
-  res.render("register", { title: "form register" })
-);
+router.get("/register", (req, res) => res.render("register", { title: "form register" }));
 router.post("/register", auth.register);
 
-router.get("/login", (req, res) =>
-  res.render("login", { title: "form login" })
-);
+router.get("/login", (req, res) => res.render("login", { title: "form login" }));
 router.post("/api/v1/auth/login", auth.akun);
 router.post("/login", auth.login);
 
